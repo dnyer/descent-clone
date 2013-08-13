8bdbe3bd6bd0122082afd6e039d760e5c528f836
David Nyer <david.nyer@gmail.com>
2013-08-13 12:03:38 -0700

    completed UpdateSpeed and UpdateAwareness functions

--- movement/movement.js
+++ movement/movement.js
@@ -1,6 +1,6 @@
 
 
-var Illumination = 1; // represents the area's illumination. Multiply by each character's 
+var Illumination = 0; // represents the area's illumination. Multiply by each character's 
 					  // Awareness trait to generate their field of view.
 					  // 0 = pitch black
 					  // 1 = starlight
@@ -14,10 +14,21 @@ function DisplaySpeedAndAwareness() {
 	document.getElementById('HeroAwareness').value = hero.attributes['Awareness'];
 }
 
-function UpdateSpeed(speed) {
+function UpdateSpeed(hero) {
+	hero.attributes['Speed'] = document.getElementById('HeroSpeed').value;
+	console.log('Speed of ' + hero.attributes['Speed'] + '\n');
+
+}
+
+function UpdateAwareness(hero) {
+	hero.attributes['Awareness'] = document.getElementById('HeroAwareness').value;
+	console.log('Awareness of ' + hero.attributes['Awareness'] + '\n');
+}
+
+function ChangeIllumination() {
 	
 }
 
-function UpdateAwareness(awareness) {
-
+function ChangeMapSize() {
+	
 }
